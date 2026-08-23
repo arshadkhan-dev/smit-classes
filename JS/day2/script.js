078b04ba7d90cdc03fe6f820b40282bcfb56ca72
arshad <arshadkhan.dev.pk>
2026-08-23 09:22:53 +0500

    variables in JS

--- JS/day2/script.js
+++ JS/day2/script.js
@@ -8,3 +8,19 @@ let a = 30;
 
 const b = 40;
 // const is a keyword, using const keyword, we must declare and assign value to a variable
+
+// Variables are containers for storing data values.
+// Define x as a variable
+let x;
+
+// Assign the value 6 to x
+x = 6;
+
+// An identifier is the name you give to a variable.
+
+// Rules for identifiers:
+
+// Must start with a letter, _, or $
+// Can contain digits after the first character
+// Cannot be a reserved keyword (let, const, if, etc.)
+// Are case-sensitive
