51be8657784eb193cf00bc1565c446c341420714
arshad <arshadkhan.dev.pk>
2026-08-29 10:55:38 +0500

    conditional statments

---
+++ JS/day5/script.js
@@ -0,0 +1,68 @@
+// Conditional Statements
+// if
+// if else
+
+// Example1
+
+// let age = Number(prompt("enter your age", 18));
+// 20;
+// if (age >= 18) {
+//   document.write("you can vote");
+// } else {
+//   document.write("you can not vote");
+// }
+
+// Example2
+
+// let age = Number(prompt("enter your age?", 18));
+// let gender = prompt("select gender", "male");
+// if (age >= 18 && gender == "male") {
+//   document.write("you can vote");
+// } else if (age >= 18 && gender == "female") {
+//   document.write("Please Visit XYZ place");
+// } else {
+//   document.write("Not Eligible");
+// }
+
+// Example 3
+
+// let role = "Admin";
+// let email = "abc@gmail.com";
+// let password = 123;
+
+// if (email === "abc@gmail.com" && password === 123) {
+//   if (role === "Admin") {
+//     document.write("You can Post");
+//   } else {
+//     document.write("You can not Post");
+//   }
+// } else {
+//   document.write("Invalid Cridentials");
+// }
+
+// Ternary Operators (Alternative of if else)
+// Syntax: condition ? "fullfilled" : "failed"
+
+// Example1
+
+// let age = Number(prompt("Enter Your Age"));
+// if (age >= 18 ? document.write("Eligible") : document.write("Not Eligable"));
+
+// Class Task
+// let marks = Number(prompt("Enter Your Marks"));
+
+// if (marks >= 90 && marks <= 100) {
+//   document.write("Welldone, your Grade is A+");
+// } else if (marks >= 80 && marks < 90) {
+//   document.write("Excllent, Your Grade is A");
+// } else if (marks >= 70 && marks < 80) {
+//   document.write("Nice, Your Grade is B");
+// } else if (marks >= 60 && marks < 70) {
+//   document.write("Good, Your Grade is C");
+// } else if (marks >= 50 && marks < 60) {
+//   document.write("Satisfactory, Your Grade is D");
+// } else if (marks >= 1 && marks < 50) {
+//   document.write("You are failed, Try Next Time");
+// } else {
+//   document.write("Enter Correct Marks");
+// }
