bda767b676aa262539797b1fdb2b49a8d1e6d37a
arshad <arshadkhan.dev.pk>
2026-09-13 11:03:20 +0500

    loop | while loop| do while

---
+++ JS/day8/loops/script.js
@@ -0,0 +1,32 @@
+// loop:
+// it execute a same block of code repeatedly based on a condition
+
+// while loop
+
+// while(condition) {
+//     // code
+//     // increment
+// }
+
+// let i = 1;
+
+// while (i <= 5) {
+//   console.log(i);
+//   i++;
+// }
+
+// let password = false;
+// let attempts = 3;
+// let i = 1;
+// while (!password && i <= attempts) {
+//   console.log("Try again");
+//   i++;
+// }
+
+// do while loop
+
+let i = 1;
+do {
+  console.log("Show Menu");
+  i++;
+} while (i <= 5);
