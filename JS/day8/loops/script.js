c5150fb9497c9ed0faf072385aafe27bcbaa70cd
arshad <arshadkhan.dev.pk>
2026-09-19 09:13:44 +0500

    while and do while loop practice

--- JS/day8/loops/script.js
+++ JS/day8/loops/script.js
@@ -25,8 +25,18 @@
 
 // do while loop
 
-let i = 1;
+// let i = 1;
+// let j = 1;
+// do {
+//   console.log(`5 * ${j} = ${i * 5}`);
+//   i++;
+//   j++;
+// } while (i <= 10);
+
+let i = 5;
+let j = 1;
 do {
-  console.log("Show Menu");
-  i++;
-} while (i <= 5);
+  console.log("5 * " + j + " = " + i);
+  i += 5;
+  j++;
+} while (i <= 50 && j <= 10);
